2e8be2be8bd11612d83c313f060df8a41abd61bd
LwandisaMasiza <166486405+LwandisaMasiza@users.noreply.github.com>
2026-08-21 11:14:51 +0200

    changed the name of the weather.js to Weather.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import weather from "./weather";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Weather App</h1>
-        <weather />
+        <Weather />
       </header>
+      <footer>
+        <p>
+          View my GitHub:
+          <a
+            href="https://github.com/LwandisaMasiza/my-app"
+            target="-blank"
+            rel="nooper noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </p>
+      </footer>
     </div>
   );
-  <footer>
-    <p>
-      View my GitHub:
-      <a
-        href="https://github.com/LwandisaMasiza/my-app"
-        target="-blank"
-        rel="nooper noreferrer"
-      >
-        GitHub Profile
-      </a>
-    </p>
-  </footer>;
 }
 
 export default App;
--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
 
-export default function weather() {
+export default function Weather() {
   <ThreeDots
     height="80"
     width="80"
@@ -14,26 +14,27 @@ export default function weather() {
     visible={true}
   />;
 
-  const [city, setCity] = useStatnpme("");
+  const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
 
   function displayWeather(response) {
     setLoaded(true);
+
     setWeather({
-      temperature: response.data.main.temperature,
-      wind: response.data.wind.speed,
+      temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      icon: "http://openweathermap.org/img/wn/${response.data.weather[0].icon}02x.png}",
-      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "54e0822b2d0f39d02a7d537obef382at";
-    let apiUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=${city}appid=${apikey}unit merit";
+
+    const apiKey = "54e0822b2d0f39d02a7d537obef382at";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
     axios.get(apiUrl).then(displayWeather);
   }
 
@@ -41,33 +42,26 @@ export default function weather() {
     setCity(event.target.value);
   }
 
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="Search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />
-      <button type="Submit">Search</button>
-    </form>
-  );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city"
+          value={city}
+          onChange={updateCity}
+        />
+        <button type="submit">Search</button>
+      </form>
 
-  if (loaded) {
-    return (
-      <div>
-        {form}{" "}
-        <ul>
-          <li>Temperature:{Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}°C</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt="weather Icon" />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return form;
-  }
+      {loaded && (
+        <div>
+          <img src={weather.icon} alt="Weather" />
+          <div>{Math.round(weather.temperature)}°C</div>
+          <div>Humidity: {weather.humidity}%</div>
+          <div>Wind: {Math.round(weather.wind)} km/h</div>
+        </div>
+      )}
+    </div>
+  );
 }
